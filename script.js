f7036218ae3de2a3c73f8d0bd5b5485290552856
Emerson Pereira <ebergp@gmail.com>
2021-09-28 14:19:35 -0300

    Adicionando função createRings

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 let tower1, tower2, tower3 = []
+let rings = 3
 
 
 // Emerson
+function createRings() {
+    for (let i = rings; i > 0; i--) {
+        const tower = document.getElementById('tower--1')
+        const ring = document.createElement('div')
+        ring.id = `ring--${i}`
+        tower.appendChild(ring)
+        console.log(ring.id)
+    }
+}
+
 
 
 // Yasmin
