91725f840686138082aa2c77967133a9c554dbb4
yasmin <yasminmartinsdebrito@gmail.com>
2021-09-28 12:06:10 -0300

    fiz a funcao de vitoria

--- script.js
+++ script.js
@@ -5,3 +5,18 @@ let tower1, tower2, tower3 = []
 
 
 // Yasmin
+let click = 0
+function condicaoVitoria(event){
+    const towerAtual = event.currentTarget;
+    const quantidadeRing = event.target.childElementCount;
+    const tower3 = document.querySelector('#tower--3')
+     
+    if( towerAtual === tower3 && quantidadeRing === 4){
+        console.log('Parabens voce venceu')
+        click = 1
+    }
+    else{
+        console.log('Click em RESET para um novo Jogo!')
+        click = 0
+    }
+}
